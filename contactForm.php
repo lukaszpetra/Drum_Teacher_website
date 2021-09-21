c4536dc9c979f877188ba4b8ab691b60a899ffad
lukaszpetra <lukaszpetra@gmail.com>
2021-09-22 00:32:19 +0200

    add goBack.html

--- contactForm.php
+++ contactForm.php
@@ -4,14 +4,17 @@ if (isset($_POST['submit'])) {
     $email = $_POST['mail'];
     //$phone=$_POST['phone'];
     $msg = $_POST['message'];
+    $sub = $_POST['subject'];
 
-    $to = 'kontakt@drummeracademy.pl'; // Receiver Email ID, Replace with your email ID
+    $to = 'kontakt@drummeracademy.pl';
+    // $to = 'lukaszpetra@gmail.com';
 
-    $message = "Wiadomość od :" . $name . "\n" . "\n" . "Treść wiadomości :" . "\n\n" . $msg;
+    $message = "Wiadomość od :" . $name . "\n" . "\n" . "\n\r" . $msg;
     $headers = "Od: " . $mail;
 
-    if (mail($to, $message, $headers)) {
-        echo "<h1>Twoja wiadomość została wysłana" . " " . $name . ", Postaramy się jak najszybciej na nią odpowiedzieć.</h1>";
+    if (mail($to, $sub, $message, $headers)) {
+        // echo "<h1>Twoja wiadomość została wysłana" . " " . $name . ", Postaramy się jak najszybciej na nią odpowiedzieć.</h1>";
+        echo '<script> alert("Twoja wiadomość została wysłana")</script>';
     } else {
         echo "Coś poszło nie tak :(";
     }
